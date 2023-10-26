3fa09ee49e98604d4f2f2581d0e8e6676c5793e9
Sri Vasavi Peravarapu <s566425@nwmissouri.edu>
2023-10-25 19:52:23 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var xx;
 var y;
 var atan;
 var exp;
@@ -9,14 +8,16 @@ var exmp1;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   
-    router.put(x=Math.random());
-    router.put(y=Math.round(x));
-    router.put (a = Math.floor(x));
-    router.post(atan=Math.atan(a));
-    router.post(exp=Math.exp(a));
-    router.post(exmp1=Math.expm1(a));
+    router.put(y=Math.random()*10);
+    router.put(x=Math.floor(y));
+    router.post(atan=Math.atan(y));
+    router.post(atan=Math.round(atan));
+    router.post(exp=Math.exp(y));
+    router.post(exp=Math.round(exp));
+    router.post(exmp1=Math.expm1(y));
+    router.post(exmp1=Math.round(exmp1));
     
-    res.send('Math.atan() applied to '+x+' is '+atan + ' \n Math.exp() applied to '+x+' is '+exp+ ' Math.exmp1() applied to '+x+' is '+exmp1);
+    res.send('Math.atan() applied to '+x+' is '+atan + ' <br> Math.exp() applied to '+x+' is '+exp+ '<br> Math.exmp1() applied to '+x+' is '+exmp1);
     
 });
 

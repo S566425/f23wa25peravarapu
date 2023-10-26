951e668bbe08e3a6b58015a1bd89a2f6e1fd0608
Sri Vasavi Peravarapu <s566425@nwmissouri.edu>
2023-10-25 19:20:51 -0500

    computation

--- routes/computation.js
+++ routes/computation.js
@@ -9,14 +9,14 @@ var exmp1;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   
-    router.put(y=Math.random()*10);
-    router.put(x=Math.round(y));
+    router.put(x=Math.random());
+    router.put(y=Math.round(x));
     router.put (a = Math.floor(x));
     router.post(atan=Math.atan(a));
     router.post(exp=Math.exp(a));
     router.post(exmp1=Math.expm1(a));
     
-    res.send('atan applied to '+x+' is '+atan + ' \n exp applied to '+x+' is '+exp+ 'exmp1 applied to '+x+' is '+exmp1);
+    res.send('Math.atan() applied to '+x+' is '+atan + ' \n Math.exp() applied to '+x+' is '+exp+ ' Math.exmp1() applied to '+x+' is '+exmp1);
     
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var sum=0;
+var more=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  router.put(more=more+1);
+  router.put(sum=sum+more);
+  res.send('Sum is: '+sum);
 });
 
 module.exports = router;
